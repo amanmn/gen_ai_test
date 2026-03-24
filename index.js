5cb7ca77ddbcb5d445f386c332bd7f6d10e6a44e
Aman Porwal <amnmandliya786@gmail.com>
2026-03-24 18:48:53 +0530

    input questions with ai

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const client = new OpenAI({
     baseURL: "https://api.groq.com/openai/v1",
 });
 
-async function run() {
+async function run(question) {
     try {
         const response = await client.responses.create({
+            instructions: "give result in 10 words",
             model: "llama-3.1-8b-instant",
-            input:"my name is aman i have calculation 200*200 ",
+            input: question,
         });
+
+        // const response = await client.responses.create({
+        //     model: "llama-3.1-8b-instant",
+        //     input: [
+        //         { role: "system", content: "You are a helpful assistant. give me only definitions." },
+        //         {role:"developer",content:"give simple definition with example"},
+        //         {
+        //             role: "user",
+        //             content: "what is debouncing and throttling in node js"
+        //         }
+        //     ]
+        // });
 
         console.log(response.output_text);
     } catch (error) {
@@ -20,5 +33,8 @@ async function run() {
     }
 }
 
-run();
-
+process.stdout.write("Ask your Question :")
+process.stdin.on("data", async (data) => {
+    const question = data.toString().trim();
+    run(question);
+});
